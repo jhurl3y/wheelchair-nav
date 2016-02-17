f20b3be15920a6b764f068fb4d06502a6dc6cc2e
James Hurley <james.hurley@exordo.com>
2016-02-17 16:17:44 +0000

    .

--- gps_poller.py
+++ gps_poller.py
@@ -15,9 +15,12 @@ class GpsPoller(threading.Thread):
         self.__gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
    
     def run(self):
-        while True:
-            self.__gpsd.next() #grab EACH set of gpsd info to clear the buffer
-            time.sleep(5) #set to whatever
+        try:
+            while True:
+                self.__gpsd.next() #grab EACH set of gpsd info to clear the buffer
+                time.sleep(5) #set to whatever
+        except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+            gpsp.join() # wait for the thread to finish what it's doing
 
     def get_location(self):
 	lat = self.__gpsd.fix.latitude
