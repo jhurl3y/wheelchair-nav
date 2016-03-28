1a4cf975afba5909b6672ed6a91b7feafe9edd0d
James Hurley <james.hurley@exordo.com>
2016-03-28 23:42:52 +0100

    ultrasonic integrated object avoidance

---
+++ ultrasonic_poller.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python
+
+import os
+from gps import *
+from time import *
+import time
+import RPi.GPIO as GPIO
+import threading
+
+class UltrasonicPoller(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__stop = threading.Event()
+        GPIO.setmode(GPIO.BOARD)
+        self.distance = 0
+   
+    def run(self):
+        try:
+            while not self.stopped():
+                GPIO.setup(21, GPIO.OUT)
+                GPIO.output(21, False)
+                time.sleep(0.000002)
+                GPIO.output(21, True)
+                time.sleep(0.000005)
+                GPIO.output(21, False)
+                GPIO.setup(21, GPIO.IN)
+
+                # Count microseconds that SIG was high
+                while GPIO.input(21) == 0:
+                  starttime = time.time()
+
+                while GPIO.input(21) == 1:
+                  endtime = time.time()
+
+                duration = endtime - starttime
+                # The speed of sound is 340 m/s or 29 microseconds per centimeter.
+                # The ping travels out and back, so to find the distance of the
+                # object we take half of the distance travelled.
+                # distance = duration / 29 / 2
+                self.distance = duration * 34000 / 2
+                time.sleep(0.5)
+
+        finally:
+            GPIO.cleanup()
+            
+    def stop(self):
+        self.__stop.set()
+
+    def stopped(self):
+        return self.__stop.isSet()
