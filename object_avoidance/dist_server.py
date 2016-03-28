e68ebb5e943d806c718105ff128da24cb2805afa
James <jhurley070@gmail.com>
2016-03-28 23:59:10 +0000

    fixed gpio

--- object_avoidance/dist_server.py
+++ object_avoidance/dist_server.py
@@ -1,12 +1,14 @@
 import time
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(23, GPIO.IN)
 try:
     while True:
         distance = GPIO.input(23)
-        print distance
+        if distance != 0:
+	    print distance
 
-    finally:
-        GPIO.cleanup()
+finally:
+    GPIO.cleanup()
   
