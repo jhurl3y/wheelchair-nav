0d1dd08b8e52c78c0450cab0388e3f8f6397a951
James Hurley <james.hurley@exordo.com>
2016-03-29 15:38:56 +0100

    .

--- object_avoidance/client.py
+++ object_avoidance/client.py
@@ -1,17 +1,35 @@
-import socket
-import sys
-
-HOST, PORT = "10.42.0.1", 9999
-data = "Hey".join(sys.argv[1:])
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-while True:
-    # As you can see, there is no connect() call; UDP has no connections.
-    # Instead, data is directly sent to the recipient via sendto().
-    sock.sendto(data + "\n", (HOST, PORT))
-    received = sock.recv(1024)
-
-    print "Sent:     {}".format(data)
-    print "Received: {}".format(received)
+'''
+    udp socket client
+    Silver Moon
+'''
+ 
+import socket   #for sockets
+import sys  #for exit
+ 
+# create dgram udp socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+ 
+host = 'localhost';
+port = 8888;
+ 
+while(1) :
+    msg = raw_input('Enter message to send : ')
+     
+    try :
+        #Set the whole string
+        s.sendto(msg, (host, port))
+         
+        # receive data from client (data, addr)
+        d = s.recvfrom(1024)
+        reply = d[0]
+        addr = d[1]
+         
+        print 'Server reply : ' + reply
+     
+    except socket.error, msg:
+        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
