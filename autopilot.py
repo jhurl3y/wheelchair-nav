d1481cfbfbd3403c53f1e444fede1e37005cb83d
James Hurley <james.hurley@exordo.com>
2016-03-31 19:42:30 +0100

    autopilot class

---
+++ autopilot.py
@@ -0,0 +1,139 @@
+#! /usr/bin/python
+
+import os
+from time import *
+import time
+import threading
+import socket
+import sys, getopt 
+sys.path.append('.') 
+import os.path 
+import math  
+from time import sleep 
+from dual_mc33926_rpi import motors, MAX_SPEED 
+import PID
+
+class Autopilot(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__stop = threading.Event()
+        self.bound = False
+        self.error = False
+
+    def wait_for_client(self, host, port):
+        try :
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print 'Socket created'
+        except socket.error, msg :
+            print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            self.error = True
+            return
+
+        try:
+            self.s.bind((host, port))
+        except socket.error , msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            self.error = True
+            return
+        
+        self.bound = True 
+        print 'Socket bind complete'
+   
+    def run(self):
+        motors.enable()
+        motors.setSpeeds(0, 0)
+
+        try:
+            P = 1.2
+            I = 1.3
+            D = 0
+            L = 200
+
+            pid = PID.PID(P, I, D)
+
+            pid.SetPoint=0
+            pid.setSampleTime(0.01)
+            motor_val = 0
+
+            END = L
+            max_out = 150.0
+            thresh_up = 0.3*MAX_SPEED
+            thresh_lo = 0.15*MAX_SPEED
+
+            while not self.stopped():
+                # receive data from client (data, addr)
+                d = self.s.recvfrom(1024)
+                data = d[0]
+                addr = d[1]
+                 
+                if not data: 
+                    continue
+                
+                data = data.strip().split()
+                print data
+
+                if int(data[1]) < 25:
+                    print 'Obstacle in way!'
+                    #motors.setSpeeds(0, 0)
+                    motors.motor1.setSpeed(-int(1.2*thresh_up))
+                    motors.motor2.setSpeed(-int(thresh_up))
+                    continue
+
+                if len(data) > 2:
+                    left = int(data[3])
+                    right = int(data[5])
+
+                    if abs(left - right) < 5:
+                        motors.motor1.setSpeed(int(1.2*thresh_up))
+                        motors.motor2.setSpeed(int(thresh_up))
+                        print 'Move Straight'
+                        continue
+
+                    feedback =  left - right
+                    pid.update(feedback)
+                    output = pid.output
+                    print 'output: ', output
+
+                    if output >= 0.0:
+                        speed = thresh_up - output/4.0 
+                    else:
+                        speed = thresh_up + output/4.0
+                    
+                    if speed < thresh_lo:
+                        drive = int(thresh_lo)  
+                    else: 
+                        drive = int(speed)
+
+                    if output <= 0.0:
+                        motors.motor1.setSpeed(int(1.2*thresh_up))
+                        motors.motor2.setSpeed(drive)
+                        print 'Move Right'
+                        print 'Left: ', int(1.2*thresh_up)
+                        print 'Right: ', drive
+
+                    else:
+                        motors.motor1.setSpeed(int(drive))
+                        motors.motor2.setSpeed(int(thresh_up))
+                        print 'Move Left'
+                        print 'Left: ', drive
+                        print 'Right: ', int(thresh_up)
+      
+        except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+            print "\nStop..."
+            motors.setSpeeds(0, 0)
+            motors.disable()
+            self.s.close()
+
+        finally:
+            print "\nStopping..."
+            motors.setSpeeds(0, 0)
+            motors.disable()
+            self.s.close()
+
+            
+    def stop(self):
+        self.__stop.set()
+
+    def stopped(self):
+        return self.__stop.isSet()
--- server.py
+++ server.py
@@ -8,118 +8,134 @@ sys.path.append('.')
 import os.path 
 
 try: 
-    while True:                 
-        server_sock = BluetoothSocket( RFCOMM )
-        server_sock.bind(("",PORT_ANY))
-        server_sock.listen(1)
+	while True:                 
+		server_sock = BluetoothSocket( RFCOMM )
+		server_sock.bind(("",PORT_ANY))
+		server_sock.listen(1)
 
-        port = server_sock.getsockname()[1]
+		port = server_sock.getsockname()[1]
 
-        uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+		uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-        advertise_service( server_sock, "SampleServer",
-                       service_id = uuid,
-                       service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                       profiles = [ SERIAL_PORT_PROFILE ], 
-                        )
+		advertise_service( server_sock, "SampleServer",
+					   service_id = uuid,
+					   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+					   profiles = [ SERIAL_PORT_PROFILE ], 
+						)
 
-        print "Waiting for connection on RFCOMM channel %d" % port
+		print "Waiting for connection on RFCOMM channel %d" % port
 
-        client_sock, client_info = server_sock.accept()
-        print "Accepted connection from ", client_info
-        nav=  None
-        start = 0.0
-        end = 0.0
-        state = 0
-        STARTED_JOURNEY = 1
-        NO_JOURNEY = 2
-        JOURNEY_PAUSED = 3
-        JOURNEY_FINISHED = 4
-        PRE_JOURNEY = 5
-        MANUAL_CONTROL = 6
+		client_sock, client_info = server_sock.accept()
+		print "Accepted connection from ", client_info
+		nav = None
+		autopilot = None
+		start = 0.0
+		end = 0.0
+		state = 0
+		STARTED_JOURNEY = 1
+		NO_JOURNEY = 2
+		JOURNEY_PAUSED = 3
+		JOURNEY_FINISHED = 4
+		PRE_JOURNEY = 5
+		MANUAL_CONTROL = 6
+		AUTONOMOUS_MODE = 7
 
-        try:
-            while True:
-                motor_driver = drive_motors.DriveMotors()
-                data = client_sock.recv(1024)
+		try:
+			while True:
+				motor_driver = drive_motors.DriveMotors()
+				data = client_sock.recv(1024)
 
-                if len(data) != 0: 
-                    data = data.split(';') 
-                    state = int(data[0])
-                print state
+				if len(data) != 0: 
+					data = data.split(';') 
+					state = int(data[0])
+					print state
 
-                if state == STARTED_JOURNEY:
-                    if data is None:
-                        continue
-                    if len(data) < 2:
-                        continue
-                    data = data[1].split()
-                    print data
+				if state == STARTED_JOURNEY:
+					if data is None:
+						continue
+					if len(data) < 2:
+						continue
+					data = data[1].split()
+					print data
 
-                    nav = gps_navigator.NAVIGATOR()
-                    start = gps_obj.GPS(float(data[0]), float(data[1]))
-                    end = gps_obj.GPS(float(data[2]), float(data[3]))
-                    nav.start_sensors(start, end, client_sock)
-                    nav.start()
-                	
-                elif state == NO_JOURNEY:
-                    continue
-                elif state == JOURNEY_PAUSED:
-                    continue
-                elif state == JOURNEY_FINISHED:
-                    if nav is not None:
-                        if not nav.stopped():
-                            print "\nKilling Thread..."
-                            nav.end_journey()
-                            nav.stop()
-                            nav.join()
-                            print "Finished Journey"
-                            break
-                elif state == PRE_JOURNEY:
-                    continue
-                elif state == MANUAL_CONTROL:
-                    motor_driver = drive_motors.DriveMotors()
-                    if nav is not None:
-                        if not nav.stopped():
-                            print "\nKilling Thread..."
-                            nav.end_journey()
-                            nav.stop()
-                            nav.join()
+					nav = gps_navigator.NAVIGATOR()
+					start = gps_obj.GPS(float(data[0]), float(data[1]))
+					end = gps_obj.GPS(float(data[2]), float(data[3]))
+					nav.start_sensors(start, end, client_sock)
+					nav.start()
+					
+				elif state == NO_JOURNEY:
+					continue
 
-                    if data is None:
-                        continue
-                    if len(data) < 2:
-                        continue
+				elif state == JOURNEY_PAUSED:
+					continue
 
-                    data = data[1]
-                    values = map(int, data.split())
-                    motor_driver.drive(values[0], values[1])
-                    print values
+				elif state == JOURNEY_FINISHED:
+					if nav is not None:
+						if not nav.stopped():
+							print "\nKilling Thread..."
+							nav.end_journey()
+							nav.stop()
+							nav.join()
+							print "Finished Journey"
+							break
 
-        except (KeyboardInterrupt): #, SystemExit): #when you press ctrl+c
-            print "\nStop..."
-            motor_driver.finish()
-            if nav is not None:
-                if not nav.stopped():
-                    print "\nKilling Thread..."
-                    nav.end_journey()
-    	     	    nav.stop()
-		    nav.join()
+				elif state == PRE_JOURNEY:
+					continue
 
-        finally:
-            print "\nStop..."
-            motor_driver.finish()
-            if nav is not None:
-                if not nav.stopped():
-                    print "\nKilling Thread..."
-                    nav.end_journey()
-                    nav.stop()
-                    nav.join()
+				elif state == MANUAL_CONTROL:
+					if nav is not None:
+						if not nav.stopped():
+							print "\nKilling Thread..."
+							nav.end_journey()
+							nav.stop()
+							nav.join()
 
-        print "Disconnected"
-        client_sock.close()
-        server_sock.close()
-        print "Finished"
+					if data is None:
+						continue
+					if len(data) < 2:
+						continue
+
+					data = data[1]
+					values = map(int, data.split())
+					motor_driver.drive(values[0], values[1])
+					print values
+
+				elif state == AUTONOMOUS_MODE:
+					if nav is not None:
+						if not nav.stopped():
+							print "\nKilling Thread..."
+							nav.end_journey()
+							nav.stop()
+							nav.join()
+							print "Finished Journey"
+							break
+
+
+		except (KeyboardInterrupt): #, SystemExit): #when you press ctrl+c
+			print "\nStop..."
+			motor_driver.finish()
+			if nav is not None:
+				if not nav.stopped():
+					print "\nKilling Thread..."
+					nav.end_journey()
+					nav.stop()
+					nav.join()
+
+		finally:
+			print "\nStop..."
+			motor_driver.finish()
+			if nav is not None:
+				if not nav.stopped():
+					print "\nKilling Thread..."
+					nav.end_journey()
+					nav.stop()
+					nav.join()
+
+		print "Disconnected"
+		client_sock.close()
+		server_sock.close()
+		print "Finished"
 
 except (KeyboardInterrupt):#, SystemExit): #when you press ctrl+c
-    print "Exiting.."
+	print "Exiting.."
